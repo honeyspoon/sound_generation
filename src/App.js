c25f692060fe796cc72e51721c8f19a17c4d048b
Abderahmane Bouziane <honeyspoon@apiary.local>
2021-12-18 09:17:37 -0500

    freq slider works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React, { useState, useEffect, useCallback, useRef } from "react";
+
+import {
+  // ElementaryPluginRenderer as core,
+  ElementaryWebAudioRenderer as core,
+  el,
+} from "@nick-thompson/elementary";
+
+import debounce from "lodash.debounce";
+
+core.on("error", (e: unknown) => {
+  console.log(e);
+});
+
+core.on("load", (e) => {
+  console.log("loaded", e);
+});
 
 function App() {
+  const [f, setF] = useState(440);
+  const [sliderF, setSliderF] = useState(f);
+
+  const debouncedSave = useRef(
+		debounce(nextValue => setF(nextValue), 1000),
+	).current;
+
+  useEffect(() => {
+    let out_left = el.mul(0.5, el.cycle(f));
+    let out_right = el.mul(0.4, el.cycle(f));
+    console.log("new f", f);
+    core.render(out_left, out_right);
+  }, [f]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>hey</p>
+      <p>
+        {f} | {sliderF}
+        <input
+          type="range"
+          min="1"
+          max="21000"
+          value={sliderF}
+          onChange={(e) => {
+            const val = parseInt(e.target.value);
+            setSliderF(val);
+            debouncedSave(sliderF);
+          }}
+        />
+      </p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {
+  // ElementaryPluginRenderer as core,
+  ElementaryWebAudioRenderer as core,
+  el,
+} from "@nick-thompson/elementary";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+let ctx = new AudioContext();
+
+function RenderApp(root) {
+  console.log(ctx);
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    root
+  );
+}
+
+core.on("load", () => {
+  const root = document.getElementById("root");
+  if (ctx.state !== "running") {
+    ReactDOM.render(
+      <React.StrictMode>
+        <div> loading ... </div>
+        <button onClick={() => ctx.resume().then(() => RenderApp(root))}>
+          load
+        </button>
+      </React.StrictMode>,
+      root
+    );
+  } else {
+    RenderApp(root);
+  }
+});
+
 reportWebVitals();
+
+async function main() {
+  let node = await core.initialize(ctx, {
+    numberOfInputs: 0,
+    numberOfOutputs: 1,
+    outputChannelCount: [2],
+  });
+  node.connect(ctx.destination);
+}
+
+main();
